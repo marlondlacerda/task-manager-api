2e1453f59975eca35bce2023705aa1f9312b2944
Marlon Lacerda <marlon@usebeon.com>
2025-06-21 18:40:19 -0300

    feat: 🦾 configuração inicial do prisma no projeto

---
+++ src/infra/repositories/prisma-user-repository.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+import { User } from "../../domain/entities/user";
+import { UserRepository } from "../../domain/repositories/user-repository";
+
+export class PrismaUserRepository implements UserRepository {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async create(user: Omit<User, 'id' | 'createdAt' | 'updatedAt'>): Promise<User> {
+    return this.prisma.user.create({ data: user });
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  async findById(id: string): Promise<User | null> {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+}
